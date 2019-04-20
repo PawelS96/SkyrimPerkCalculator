1fb0fe72d5c6a9d383979e543fcdeaff33a90fa7
Pawel <psob96@gmail.com>
2019-04-21 01:00:23 +0200

    0.9

---
+++ app/src/main/java/com/pawels96/skyrimperkcalculator/ui/LoopingViewPager.java
@@ -0,0 +1,55 @@
+package com.pawels96.skyrimperkcalculator.ui;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+public class LoopingViewPager extends ViewPager {
+    public LoopingViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public interface MovementListener{
+        void onActionDown();
+    }
+
+    private MovementListener listener;
+
+    public void setListener(MovementListener listener){
+        this.listener = listener;
+    }
+
+    public void enableLoop(final int pages) {
+
+        addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            int previousState, currentState;
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+                if (state == 1)
+                    listener.onActionDown();
+
+                int currentPage = getCurrentItem();
+                if (currentPage == pages - 1 || currentPage == 0) {
+                    previousState = currentState;
+                    currentState = state;
+                    if (previousState == 1 && currentState == 0) {
+                        setCurrentItem(currentPage == 0 ? pages - 1 : 0, false);
+                    }
+                }
+            }
+        });
+    }
+}
