ce3904dbaf3386cf4c0ebe591fd74ece974d9b06
Pawel <psob96@gmail.com>
2020-08-23 23:00:26 +0200

    Added tests

---
+++ app/src/androidTest/java/com/pawels96/skyrimperkcalculator/DatabaseMigrationTest.kt
@@ -0,0 +1,64 @@
+package com.pawels96.skyrimperkcalculator
+
+import android.content.Context
+import androidx.test.InstrumentationRegistry
+import com.pawels96.skyrimperkcalculator.data.AppDatabase
+import com.pawels96.skyrimperkcalculator.data.OldDatabase
+import com.pawels96.skyrimperkcalculator.data.Repository
+import com.pawels96.skyrimperkcalculator.domain.Build
+import com.pawels96.skyrimperkcalculator.domain.PerkSystem
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class DatabaseMigrationTest {
+
+    val context: Context = InstrumentationRegistry.getTargetContext()
+
+    private var newDb = AppDatabase.getInMemoryDatabase(context)
+    private val repo = Repository(newDb.buildDAO())
+    private val oldDb: OldDatabase = OldDatabase(context, repo)
+
+    @Before
+    fun bef() {
+        oldDb.clear()
+        oldDb.onDowngrade(oldDb.writableDatabase, 4, 3)
+        newDb = AppDatabase.getInMemoryDatabase(context)
+        newDb.clearAllTables()
+    }
+
+    private fun checkEquality_excludeID(build1: Build, build2: Build) {
+        assertEquals(build1.name, build2.name)
+        assertEquals(build1.description, build2.description)
+        assertEquals(build1.system, build2.system)
+        assertEquals(build1.skills, build2.skills)
+        assertEquals(build1.werewolfSkill, build2.werewolfSkill)
+        assertEquals(build1.werewolfPerkSystem, build2.werewolfPerkSystem)
+        assertEquals(build1.vampireSkill, build2.vampireSkill)
+        assertEquals(build1.vampirePerkSystem, build2.vampirePerkSystem)
+    }
+
+    @Test
+    fun testMigration() {
+
+        val builds = PerkSystem.values().mapIndexed { index, perkSystem -> Build.create(perkSystem) }
+        builds.forEach { oldDb.saveBuild(it) }
+        val fromOldDB = PerkSystem.values().map { oldDb.getAllBuilds(it) }.toMutableList().flatten()
+
+        builds.zip(fromOldDB).forEach {
+            checkEquality_excludeID(it.first, it.second)
+        }
+
+        oldDb.onUpgrade(oldDb.writableDatabase,3, 4)
+
+        val fromNewDB = PerkSystem.values().map { repo.getByPerkSystem(it, false) }.toMutableList().flatten()
+
+        assertEquals(fromOldDB.size, fromNewDB.size)
+
+        fromOldDB.zip(fromNewDB).forEach {
+            checkEquality_excludeID(it.first, it.second)
+        }
+    }
+
+
+}
---
+++ app/src/androidTest/java/com/pawels96/skyrimperkcalculator/RepositoryTest.kt
@@ -0,0 +1,100 @@
+package com.pawels96.skyrimperkcalculator
+
+import android.content.Context
+import com.pawels96.skyrimperkcalculator.data.AppDatabase
+import org.junit.After
+import androidx.test.InstrumentationRegistry
+import com.pawels96.skyrimperkcalculator.domain.Build
+import com.pawels96.skyrimperkcalculator.data.BuildMapper
+import com.pawels96.skyrimperkcalculator.data.Repository
+import com.pawels96.skyrimperkcalculator.domain.PerkSystem
+import com.pawels96.skyrimperkcalculator.domain.VampirePerkSystem
+import com.pawels96.skyrimperkcalculator.domain.enums.EMainSkill
+import com.pawels96.skyrimperkcalculator.domain.skills_vanilla.Alchemy
+import com.pawels96.skyrimperkcalculator.presentation.Utils.DEFAULT_BUILD_NAME
+import junit.framework.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class RepositoryTest {
+
+    val context: Context = InstrumentationRegistry.getTargetContext()
+    var db = AppDatabase.getInMemoryDatabase(context)
+    var repo = Repository(db.buildDAO())
+
+    @Before
+    fun reset(){
+        db = AppDatabase.getInMemoryDatabase(context)
+        repo = Repository(db.buildDAO())
+    }
+
+    @Test
+    fun insertAndGetBuildByPerkSystem(){
+
+        val build = Build.create(PerkSystem.VANILLA, 1)
+        build.getSkill(EMainSkill.SKILL_ALCHEMY)[Alchemy.VAN_ALC_PURITY]!!.nextState()
+        repo.insert(build)
+
+        val fromDb = repo.getByPerkSystem(PerkSystem.VANILLA, false)[0]
+        assertEquals(build, fromDb)
+
+        build.getSkill(EMainSkill.SKILL_ALCHEMY)[Alchemy.VAN_ALC_PURITY]!!.nextState()
+        assertNotSame(build, fromDb)
+    }
+
+    @Test
+    fun insertAndCheckNameAvailability(){
+
+        val ps = PerkSystem.VANILLA
+        val nname = "test"
+
+        val build = Build.create(ps, 1).apply { name = nname }
+        repo.insert(build)
+        assertFalse(repo.isNameAvailable(nname,ps ))
+    }
+
+    @Test
+    fun getByInvalidName_shouldInsertAndReturnDefaultBuild(){
+        assertEquals(DEFAULT_BUILD_NAME, repo.getByNameOrDefault("invalid", PerkSystem.VANILLA).name)
+    }
+
+    @Test
+    fun getFromEmptyDatabase_shouldInsertAndReturnDefaults(){
+        assertEquals(0, repo.count())
+        val fromDB = PerkSystem.values().map { repo.getByPerkSystem(it, true)  }.toList().flatten()
+        assertEquals(PerkSystem.values().toList(), fromDB.map { it.system }.toList())
+        assertEquals(PerkSystem.values().size, repo.count())
+    }
+
+    @Test
+    fun changeVampirePerkSystemTest(){
+        val ps = PerkSystem.VOKRII
+
+        repo.insert(Build.create(ps).apply { name = "test"; vampirePerkSystem = VampirePerkSystem.VANILLA })
+        var fromDb = repo.getByPerkSystem(ps, false)[0]
+
+        fromDb.vampirePerkSystem = VampirePerkSystem.SACROSANCT
+        repo.update(fromDb)
+
+        var fromDb2 = repo.getByPerkSystem(ps, false)[0]
+        assertEquals(VampirePerkSystem.SACROSANCT, fromDb2.vampirePerkSystem)
+    }
+
+    @Test
+    fun rename(){
+
+        val oldName = "test"
+        val ps = PerkSystem.VANILLA
+        repo.insert(Build.create(ps).apply { name = oldName })
+
+        var fromDb = repo.getByPerkSystem(ps, false)[0]
+        fromDb.name = "newName"
+        val updated = repo.update(fromDb)
+        assert(updated)
+
+        val fromDb2 = repo.getByPerkSystem(ps, false)[0]
+        assertEquals("newName", fromDb2.name)
+        assertEquals(true, repo.isNameAvailable(oldName, ps ))
+    }
+
+}
