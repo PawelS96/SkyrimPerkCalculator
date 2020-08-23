4da7afe4f15088e1ef80abaaca1739efc64cfa3c
Pawel <psob96@gmail.com>
2020-08-23 22:53:05 +0200

    Refactor

--- app/src/main/java/com/pawels96/skyrimperkcalculator/presentation/views/LoopingViewPager.java
+++ app/src/main/java/com/pawels96/skyrimperkcalculator/presentation/views/LoopingViewPager.java
@@ -1,9 +1,10 @@
-package com.pawels96.skyrimperkcalculator.ui;
+package com.pawels96.skyrimperkcalculator.presentation.views;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.view.ViewPager;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
 import android.util.AttributeSet;
 
 /**
