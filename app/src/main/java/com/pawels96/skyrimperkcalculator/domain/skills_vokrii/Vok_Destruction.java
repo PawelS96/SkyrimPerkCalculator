c600e736ea895118f9995eedca66008ebc5c2dd7
PawelS96 <pawelsobieszczuk@wp.pl>
2021-07-18 11:46:19 +0200

    Update perks in mods

--- app/src/main/java/com/pawels96/skyrimperkcalculator/domain/skills_vokrii/Vok_Destruction.java
+++ app/src/main/java/com/pawels96/skyrimperkcalculator/domain/skills_vokrii/Vok_Destruction.java
@@ -10,7 +10,8 @@ public enum Vok_Destruction implements IPerk {
     VOK_DES_AUGMENTED_FLAMES(0.225f, 0.65f, 30, 60),
     VOK_DES_AUGMENTED_FROST(0.45f, 0.575f, 30, 60),
     VOK_DES_AUGMENTED_SHOCK(0.65f, 0.625f, 30, 60),
-    VOK_DES_RUNE_MASTER(0.85f, 0.675f, 40),
+    VOK_DES_RUNE_MASTER(0.85f, 0.675f, 40, 70),
+    VOK_DES_RAW_POWER(0.85f, 0.8f, 30, 60, 90),
     VOK_DES_DEVOURING_FLAMES(0.15f, 0.4f, 50),
     VOK_DES_CHILLING_FROST(0.35f, 0.35f, 50),
     VOK_DES_DEAFENING_SHOCK(0.6f, 0.35f, 50),
@@ -22,7 +23,6 @@ public enum Vok_Destruction implements IPerk {
     VOK_DES_ELEMENTAL_BARRIER(0.75f, 0.25f, 80),
     VOK_DES_ELEMENTAL_SHIELD(0.9f, 0.3f, 90),
     VOK_DES_HELLSTORM(0.4f, 0.05f, 100);
-    // VOK_DES_EYE_OF_THE_STORM(0.8f, 0.1f, 100);
 
     Vok_Destruction(float x, float y, int... skill) {
         info = new PerkInfo(skill, x, y);
