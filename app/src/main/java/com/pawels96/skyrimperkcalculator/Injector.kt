416411996b640ac40b9669bbf75f3e589a2c9483
PawelS96 <pawelsobieszczuk@wp.pl>
2023-06-07 21:13:32 +0200

    refactor: delete old database code

--- app/src/main/java/com/pawels96/skyrimperkcalculator/Injector.kt
+++ app/src/main/java/com/pawels96/skyrimperkcalculator/Injector.kt
@@ -3,13 +3,11 @@ package com.pawels96.skyrimperkcalculator
 import android.content.Context
 import com.pawels96.skyrimperkcalculator.data.AppDatabase
 import com.pawels96.skyrimperkcalculator.data.DefaultBuildRepository
-import com.pawels96.skyrimperkcalculator.data.OldDatabase
 import com.pawels96.skyrimperkcalculator.data.Preferences
 import com.pawels96.skyrimperkcalculator.presentation.viewmodels.BuildsViewModel
 
 object Injector {
 
-    lateinit var oldDb: OldDatabase
     lateinit var prefs: Preferences
 
     private lateinit var db: AppDatabase
@@ -19,7 +17,6 @@ object Injector {
         db = AppDatabase.getDatabase(context)
         prefs = Preferences(context.getSharedPreferences("prefs", Context.MODE_PRIVATE))
         repo = DefaultBuildRepository(db.buildDAO())
-        oldDb = OldDatabase(context, repo)
     }
 
     fun provideVmFactory() = BuildsViewModel.Factory(repo, prefs)
