7669585738fbc86e2c740904ef23b015a68012bc
PawelS96 <pawelsobieszczuk@wp.pl>
2023-06-16 22:41:58 +0200

    update: verify string resources on app launch in debug mode

--- app/src/main/java/com/pawels96/skyrimperkcalculator/App.kt
+++ app/src/main/java/com/pawels96/skyrimperkcalculator/App.kt
@@ -1,11 +1,16 @@
 package com.pawels96.skyrimperkcalculator
 
 import android.app.Application
+import com.pawels96.skyrimperkcalculator.presentation.common.verifyStringResources
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Injector.init(this)
+
+        if (BuildConfig.DEBUG) {
+            verifyStringResources()
+        }
     }
 }
--- app/src/main/java/com/pawels96/skyrimperkcalculator/presentation/common/StringResourceUtils.kt
+++ app/src/main/java/com/pawels96/skyrimperkcalculator/presentation/common/StringResourceUtils.kt
@@ -2,11 +2,17 @@ package com.pawels96.skyrimperkcalculator.presentation.common
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.util.Log
+import com.pawels96.skyrimperkcalculator.domain.EMainSkill
+import com.pawels96.skyrimperkcalculator.domain.ESpecialSkill
 import com.pawels96.skyrimperkcalculator.domain.IPerk
+import com.pawels96.skyrimperkcalculator.domain.IPerkSystem
 import com.pawels96.skyrimperkcalculator.domain.ISkill
 import com.pawels96.skyrimperkcalculator.domain.PerkSystem
+import com.pawels96.skyrimperkcalculator.domain.VampirePerkSystem
+import com.pawels96.skyrimperkcalculator.domain.WerewolfPerkSystem
 
-fun PerkSystem.getName(context: Context): String {
+fun IPerkSystem.getName(context: Context): String {
     val resName = "s_" + this.toString().lowercase()
     return context.getString(resName, resName)
 }
@@ -35,7 +41,34 @@ private fun Context.getString(resName: String, defaultValue: String): String {
         val id = resources.getIdentifier(resName, "string", packageName)
         resources.getString(id)
     } catch (e: Exception) {
-        e.printStackTrace()
+        Log.e("resources", "Failed to get string: $resName")
         defaultValue
+    }
+}
+
+fun Context.verifyStringResources() {
+    val allPerkSystems = PerkSystem.values().toList() + VampirePerkSystem.values().toList() + WerewolfPerkSystem.values().toList()
+    allPerkSystems.forEach { it.getName(this) }
+
+    val allSkills = EMainSkill.values().toList() + ESpecialSkill.values().toList()
+    allSkills.forEach { it.getName(this) }
+
+    val allMainPerks = PerkSystem.values()
+        .flatMap { system ->
+            EMainSkill.values().flatMap { it.getPerks(system).toList() }
+        }
+    val allVampirePerks = VampirePerkSystem.values()
+        .flatMap { system ->
+            ESpecialSkill.SKILL_VAMPIRISM.getPerks(system).toList()
+        }
+    val allWerewolfPerks = WerewolfPerkSystem.values()
+        .flatMap { system ->
+            ESpecialSkill.SKILL_LYCANTHROPY.getPerks(system).toList()
+        }
+
+    val allPerks = allMainPerks + allVampirePerks + allWerewolfPerks
+    allPerks.forEach {
+        it.getName(this)
+        it.getDescription(this)
     }
 }
